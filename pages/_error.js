a59318a4f4ef9578b1173339f6eb45ade3930f51
jriley15 <jordanr3@live.com>
2019-08-03 21:48:46 -1000

    404 v1

--- pages/_error.js
+++ pages/_error.js
@@ -1,7 +1,53 @@
 import React from "react"
+import { Button, Typography, Grid } from "@material-ui/core"
+import Link from "next/link"
+import { makeStyles } from "@material-ui/styles"
+import NavBar from "../components/Navbar"
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: "calc(100vh - 104px)",
+  },
+  container: {
+    height: "inherit",
+  },
+  paddingTop5: {
+    padding: theme.spacing(5),
+  },
+}))
 
 const error = () => {
-  return <>Page not found</>
+  const classes = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <NavBar />
+      <Grid
+        container
+        justify="center"
+        className={classes.container}
+        alignItems="center"
+        alignContent="center"
+        direction="column"
+      >
+        <Grid item>
+          <Typography
+            variant="h1"
+            align="center"
+            color="error"
+            className={classes.paddingTop5}
+          >
+            404 Page not found
+          </Typography>
+        </Grid>
+        <Grid item className={classes.paddingTop5}>
+          <Link href="/">
+            <Button>Go To Home Page</Button>
+          </Link>
+        </Grid>
+      </Grid>
+    </div>
+  )
 }
 
 export default error
