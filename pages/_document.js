be23c231f33bf9c16b3a60c02aedf1791ac5329b
jriley15 <jordanr3@live.com>
2019-08-14 18:20:11 -0700

    html static form setup

--- pages/_document.js
+++ pages/_document.js
@@ -27,6 +27,11 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
+          <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+            <input type="text" name="name" />
+            <input type="email" name="email" />
+            <textarea name="message" />
+          </form>
           <Main />
           <NextScript />
         </body>
