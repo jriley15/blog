b3ec8d15eb87c95907b1684c73ffd90bfdb74b1e
jriley15 <jordanr3@live.com>
2019-08-18 13:36:30 -0700

    cd preview test

--- data/blog.js
+++ data/blog.js
@@ -3,13 +3,18 @@ const client = require("contentful").createClient({
   accessToken: "dyYpdf5QLOErJecEzpBQDxEJWOj4Pj8W71aU3gedPCY", //process.env.ACCESS_TOKEN
 })
 
+const sanityClient = require("@sanity/client")({
+  projectId: "3589as9m", // you can find this in sanity.json
+  dataset: "projects", // or the name you chose in step 1
+  useCdn: true, // `false` if you want to ensure fresh data
+})
+
 module.exports = {
   getPosts: async function() {
     const posts = await client.getEntries({
       content_type: "blogPost",
     })
     return posts.items
-
   },
   getPost: async function(id) {
     const post = await client.getEntry(id)
@@ -17,7 +22,5 @@ module.exports = {
     return post
   },
 
-  getSanityPosts: async function(id) {
-    
-  }
+  getSanityPosts: async function(id) {},
 }
