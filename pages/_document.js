8895088a2eea41cdc543a338abc184779fc2a77c
jriley15 <jordanr3@live.com>
2019-08-18 21:11:11 -0700

    final form fix

--- pages/_document.js
+++ pages/_document.js
@@ -28,18 +28,6 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
-          <form
-            name="contact"
-            method="post"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-          >
-            {/* You still need to add the hidden input with the form name to your JSX form */}
-            <input type="hidden" name="form-name" value="contact" />
-            <input type="text" name="name" />
-            <input type="email" name="email" />
-            <textarea name="message" />
-          </form>
           <Main />
           <NextScript />
         </body>
