4d791995885b73dded2faa2f52c5762666e8f990
jriley15 <jordanr3@live.com>
2019-08-18 18:51:41 -0700

    cdn fix?

--- data/blog.js
+++ data/blog.js
@@ -1,7 +1,7 @@
 const sanityClient = require("@sanity/client")({
   projectId: "3589as9m", // you can find this in sanity.json
   dataset: "portfolio", // or the name you chose in step 1
-  useCdn: false, // `false` if you want to ensure fresh data
+  useCdn: true, // `false` if you want to ensure fresh data
 })
 
 const imageUrlBuilder = require("@sanity/image-url")
